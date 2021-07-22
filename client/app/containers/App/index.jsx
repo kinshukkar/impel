b92e89dfa59d33535674557e45341e3db95b608b
POMPITA SARKAR <pompitasarkar26@gmail.com>
2021-07-22 11:11:25 +0530

    UI enhancements; Strava routing back to app

--- client/app/containers/App/index.jsx
+++ client/app/containers/App/index.jsx
@@ -47,7 +47,7 @@ const App = (props) => {
         titleTemplate="%s"
         defaultTitle="Impel"
       >
-        <title>Impel - Get Fitter and Richer</title>
+        <title>Impel - Get Fitter with Rewards</title>
       </Helmet>
       <Suspense fallback={<div>Loading...</div>}>
         {
